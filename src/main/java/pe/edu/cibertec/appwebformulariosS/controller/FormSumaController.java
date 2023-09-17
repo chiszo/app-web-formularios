aee47960a2b2f0c98227decd9f52cf73c2574690
chiszo <gigibethandrea@gmail.com>
2023-09-17 12:05:28 -0500

    desarrollo de promedio

--- src/main/java/pe/edu/cibertec/appwebformulariosS/controller/FormSumaController.java
+++ src/main/java/pe/edu/cibertec/appwebformulariosS/controller/FormSumaController.java
@@ -1,13 +1,36 @@
 package pe.edu.cibertec.appwebformulariosS.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.cibertec.appwebformulariosS.model.PromedioModel;
 
 @Controller
 public class FormSumaController {
 
     @GetMapping("/frmSuma")
-    public String Index(){
+    public String index(Model model){
+        model.addAttribute("verresultado",false);
+        model.addAttribute("promediomodel", new PromedioModel());
+        return "frmSuma";
+    }
+
+    @PostMapping("/promedionotas")
+    public String calcularPromedioNotas(
+            @ModelAttribute("promediomodel") PromedioModel promedioModel,
+            Model model){
+        Double promedio = (promedioModel.getCl1()*0.15)
+                + (promedioModel.getCl2()*0.2)
+                + (promedioModel.getCl3()*0.35)
+                + (promedioModel.getProyfin()*0.3);
+        String estado = promedio>10.49 ? "Aprobado" : "Desaprobado";
+        model.addAttribute("resultado","su promedio de notas es: "
+                + promedio+", usted se encuentra "
+                + estado);
+        model.addAttribute("verresultado",true);
+        model.addAttribute(new PromedioModel());
         return "frmSuma";
     }
 }
---
+++ src/main/java/pe/edu/cibertec/appwebformulariosS/model/PromedioModel.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.appwebformulariosS.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class PromedioModel {
+    private Integer cl1;
+    private Integer cl2;
+    private Integer cl3;
+    private Integer proyfin;
+}
