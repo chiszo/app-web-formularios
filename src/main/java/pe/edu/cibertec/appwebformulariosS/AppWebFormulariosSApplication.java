08193ae5416026c51f7af7b841d39998c63571b4
chiszo <gigibethandrea@gmail.com>
2023-09-17 09:35:04 -0500

    Primer commit

---
+++ src/main/java/pe/edu/cibertec/appwebformulariosS/AppWebFormulariosSApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebformulariosS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFormulariosSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFormulariosSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebformulariosS/AppWebFormulariosSApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebformulariosS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFormulariosSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
