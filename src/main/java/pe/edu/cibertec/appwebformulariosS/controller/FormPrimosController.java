2597152b7442a0035db15d0526106a5d2024568b
chiszo <gigibethandrea@gmail.com>
2023-09-24 12:22:24 -0500

    ejercicio3 terminado

--- src/main/java/pe/edu/cibertec/appwebformulariosS/controller/FormPrimosController.java
+++ src/main/java/pe/edu/cibertec/appwebformulariosS/controller/FormPrimosController.java
@@ -1,4 +1,39 @@
 package pe.edu.cibertec.appwebformulariosS.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.cibertec.appwebformulariosS.model.Primo;
+
+@Controller
 public class FormPrimosController {
+
+    public Boolean esPrimo(Integer numero){
+        Integer contador = 0;
+        for(int i = 1; i<=numero; i++){
+            if((numero%i)==0){
+                contador++;
+            }
+        }
+        return contador<=2;
+    }
+
+    @GetMapping("/frmPrimos")
+    public String primos(Model model){
+        model.addAttribute("verresultado", false);
+        model.addAttribute("Primo", new Primo());
+        return "frmPrimos";
+    }
+
+    @PostMapping("calcularprimo")
+    public String calcularprimo(@ModelAttribute("Primo")Primo primo, Model model){
+        String resultado= esPrimo(primo.getNumero())
+                ? "El número " + primo.getNumero()+" es PRIMO"
+                :"El número " + primo.getNumero()+" NO es PRIMO";
+        model.addAttribute("verresultado",true);
+        model.addAttribute("resultado", resultado);
+        return "frmPrimos";
+    }
 }
---
+++ src/main/java/pe/edu/cibertec/appwebformulariosS/model/Primo.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebformulariosS.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Primo {
+    private Integer numero;
+}
